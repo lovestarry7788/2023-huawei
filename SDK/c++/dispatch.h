0c1cdfbfd83230e0ed021a87da10a3bdd77572b7
kelatte <kelatte@outlook.com>
2023-03-12 21:53:14 +0800

    修正语法错误

--- SDK/c++/dispatch.h
+++ SDK/c++/dispatch.h
@@ -4,13 +4,14 @@
 #include "input.h"
 #include "geometry.h"
 #include "output.h"
+#include <set>
 
 // 负责运送机器人过程，包含避障
 // 先购买/出售，再addplan，再调用FrameEnd交给dispatch处理topoint
 namespace Dispatch {
     using namespace Geometry;
-    extern vector<set<Point>> forecast_;
-    extern vector<Point> plan_;
+    extern std::vector<std::set<Point>> forecast_;
+    extern std::vector<Point> plan_;
     void AddPlan(int robot_id, int workbench_id);
     void FrameEnd();
 }
