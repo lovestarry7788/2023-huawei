6b307c45390c34742a2cb9ee3d4f5f72a55cc1c8
lovestarry7788 <jay.lau2020.work@outlook.com>
2023-03-24 10:42:09 +0800

    feat: 增强1号图

--- SDK/c++/main.cpp
+++ SDK/c++/main.cpp
@@ -351,8 +351,30 @@ namespace Solution1 {
         return false;
     }
 
+    void SetConfig() {
+        switch (map_number_) {
+            case 1: // 加重生产 7 的速度
+                sever_one = 2.0;
+                four_five_six_one = 1.5;
+                sever_two = 1.8;
+                four_five_six_two = 1.2;
+                sever_three = 1.0;
+                premium_coefficient[1] = 1.5;
+                premium_coefficient[2] = 3;
+                break;
+            default:
+                sever_one = 2.0;
+                four_five_six_one = 1.5;
+                sever_two = 1.2;
+                four_five_six_two = 1.2;
+                sever_three = 1.0;
+                break;
+        }
+    }
+
     void Solve() {
         Input::ScanMap();
+        SetConfig(); // 针对地图设置参数
         // Config_Read_From_Files(); // 搜参数专用
         while(Input::ScanFrame()) {
             Init();
