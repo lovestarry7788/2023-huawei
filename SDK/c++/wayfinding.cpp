6e91de7987d021055d89c95594c3959a423c18ba
lovestarry7788 <jay.lau2020.work@outlook.com>
2023-04-07 11:45:38 +0800

    feat: 判断障碍

--- SDK/c++/wayfinding.cpp
+++ SDK/c++/wayfinding.cpp
@@ -21,13 +21,81 @@ std::vector<double> WayFinding2::Unique_y[2];//用于离散化y坐标
 double WayFinding2::Dis[2][50][N_];//最短路： 初始建图的最短路
 double WayFinding2::Dis2[2][50][N_];//最短路2：
 int WayFinding2::From[2][50][N_][15];//状态， 工作台编号， 终点， 2^k步： 从终点出发到对应的工作台的最短路上， 走2^k到达哪个点
+std::vector<double> WayFinding2::joint_obs_x0_;
+std::vector<double> WayFinding2::joint_obs_[101];
 
 int WayFinding2::FindRoute(int o, int A, int B) {
     return -1;//表示没有找到P点
 }
 void WayFinding2::Unique(std::vector<double> &a);//离散化a
 int WayFinding2::GetID(double x_, double y_);//获得x,y对应的离散化id
-void WayFinding2::Insert_Edge(int o, int u, int v, double dis);
-void WayFinding2::Init();//拆解地图， 每一个格子都拆成K_个点， K_往4个方向联通
-void WayFinding2::Dijkstra(int o, int s);//有没有拿东西o:0/1, 起点： 工作台
-bool WayFinding2::Check_Avoid_Crash_Wall(int o, int A, int B);//测试沿着AB连线走会不会撞墙
+void WayFinding2::Insert_Edge(int o, int u, int v, double dis) {
+    len[o] ++;
+    edge[o][len[o]] = Edge{u, v, head[o][u], dis};
+    head[o][u] = len[o];
+}
+void WayFinding2::Init() { //拆解地图， 每一个格子都拆成K_个点， K_往4个方向联通
+
+    for(int i = 0; i < map_size_; ++i) {
+        for(int j = 0; j < map_size_; ++j) {
+            if(map_[i][j] == '#') {
+                double px = j * 0.5 + 0.25;
+                joint_obs_x0_.push_back(px + 0.25);
+                joint_obs_x0_.push_back(px - 0.25);
+            }
+        }
+    }
+
+    /*
+     * 对所有的障碍点集进行去重
+     */
+    Unique(joint_obs_x0_);
+    for (int i = 0; i < map_size_; i++) {
+        for (int j = 0; j < map_size_; j++) if(map_[i][j] == '#'){
+                double px = j * 0.5 + 0.25;
+                double py = (map_size_ - i - 1) * 0.5 + 0.25;
+                int idx;
+                idx = std::lower_bound(joint_obs_x0_.begin(), joint_obs_x0_.end(), px + 0.25) - joint_obs_x0_.begin();
+                joint_obs_[idx].push_back(py + 0.25);
+                joint_obs_[idx].push_back(py - 0.25);
+
+                idx = std::lower_bound(joint_obs_x0_.begin(), joint_obs_x0_.end(), px - 0.25) - joint_obs_x0_.begin();
+                joint_obs_[idx].push_back(py + 0.25);
+                joint_obs_[idx].push_back(py - 0.25);
+            }
+    }
+
+}
+void WayFinding2::Dijkstra(int o, int s) { //有没有拿东西o:0/1, 起点： 工作台
+
+}
+
+bool WayFinding2::Check_Avoid_Crash_Wall(int o, int A, int B) { //测试沿着AB连线走会不会撞墙
+    double x0, x1, y0, y1;
+    int x0_, x1_, y0_, y1_;
+    double d;
+    auto a = Point_[o][A];
+    auto b = Point_[o][B]; // 找到 a, b 之间的路径
+    bool valid = true;
+    double mind = 1e18;
+    double radius = Radius[o];
+    x0 = std::min(a.x, b.x) - radius, x1 = std::max(a.x, b.x) + radius, y0 = std::min(a.y, b.y) - radius, y1 = std::max(a.y, b.y) + radius;
+    x0_ = std::lower_bound(joint_obs_x0_.begin(), joint_obs_x0_.end(), x0) - joint_obs_x0_.begin() - 1; x0_ = std::max(x0_, 0);
+    x1_ = std::lower_bound(joint_obs_x0_.begin(), joint_obs_x0_.end(), x1) - joint_obs_x0_.begin();
+    for (int x = x0_; x <= x1_; ++x) if(!joint_obs_[x].empty()){
+            y0_ = std::lower_bound(joint_obs_[x].begin(), joint_obs_[x].end(), y0) - joint_obs_[x].begin() - 1; y0_ = std::max(y0_, 0);
+            y1_ = std::lower_bound(joint_obs_[x].begin(), joint_obs_[x].end(), y1) - joint_obs_[x].begin();
+            for (int y = y0_; y <= y1_; ++y) {
+                d = DistanceToSegment({joint_obs_x0_[x], joint_obs_[x][y]}, a, b);
+                //                        if(j == 71) Log::print("Radius: ", Radius[o], "i: ", i, "j: ", j, "a.x: ", a.x, "a.y: ", a.y, "b.x: ", b.x, "b.y: ",b.y, "x0: ",joint_obs_x0_[x], "y0: ", joint_obs_[x][y], "d: ", d);
+                mind = std::min(mind, d);
+                // 细小问题，两点间直线，只有一个瓶颈，中间有空的，仍可以通过多辆车。no，没问题，将防碰撞提前处理了部分。
+                if (d < radius + 2e-2) { // 操作误差
+                    valid = false;
+                    break;
+                }
+            }
+            if(!valid) break;
+        }
+    return valid;
+}
--- SDK/c++/wayfinding.h
+++ SDK/c++/wayfinding.h
@@ -20,7 +20,7 @@ namespace WayFinding2 {
         double dis; // 距离
         double dis_to_wall; // 离墙4端点最近距离
         Edge(){}
-        Edge(int _u, int _v, int _nex, double _dis, double _dis_to_wall) : u(_u), v(_v), nex(_nex), dis(_dis), dis_to_wall(_dis_to_wall){}
+        Edge(int _u, int _v, int _nex, double _dis) : u(_u), v(_v), nex(_nex), dis(_dis){}
     };
 
     extern int cnt;//建边， 边的数量
@@ -55,10 +55,20 @@ namespace WayFinding2 {
 
     };
 
+    extern double Radius[2] = {0.45, 0.53};
     extern double Dis[2][50][N_];//最短路： 初始建图的最短路
     extern double Dis2[2][50][N_];//最短路2：
     extern int From[2][50][N_][15];//状态， 工作台编号， 终点， 2^k步： 从终点出发到对应的工作台的最短路上， 走2^k到达哪个点
+    extern std::vector<double> joint_obs_x0_;
+    extern std::vector<double> joint_obs_[101];
+    extern double dist[2][N_][N_];
+    extern int pre[2][N_][N_];
+    extern Edge edge[2][N_ * 20];
+    extern int len[2];
+    extern int head[2][N_];
+
     void Init();//拆解地图， 每一个格子都拆成K_个点， K_往4个方向联通
+    void Insert_Edge(int o, int u, int v, double dis);
     void Dijkstra(int o, int s);//有没有拿东西o:0/1, 起点： 工作台
     int FindRoute(int o, int A, int B);//倍增， 找一个AB路径中离A最远的P， 使得A能直线到P
     bool Check_Avoid_Crash_Wall(int o, int A, int B);//测试沿着AB连线走会不会撞墙
